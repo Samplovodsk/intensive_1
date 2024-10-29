2b37c47939c0486ee41336d127c29f9ba26bbb7b
pipe bomber <150795672+diisorderedguy@users.noreply.github.com>
2024-10-29 21:33:54 +0300

    чисто ну такой
    переделал комплект данных чтобы чистенько всё было (и лучше читалось)

--- peepeepoopoo/scriptsPy/parsing.py
+++ peepeepoopoo/scriptsPy/parsing.py
@@ -1,6 +1,16 @@
 import cianparser
+import pandas as pd
+import time
 
-moscow_parser = cianparser.CianParser(location="Москва")
-data = moscow_parser.get_flats(deal_type="sale", rooms=(1), with_saving_csv=True, additional_settings={"start_page":6, "end_page":10})
+moscow = cianparser.CianParser(location='Москва')
 
-print(data[0])
+data = []
+for i in range(1, 20):
+    a = moscow.get_flats(deal_type='sale', rooms=(5), additional_settings={'start_page': i, 'end_page':i} )
+    data.extend(a)
+    time.sleep(5)
+    
+exel_export = pd.DataFrame(data)
+columns = ['author', 'author_type', 'location', 'deal_type', 'accommodation_type', 'floors_count', 'rooms_count', 'total_meters', 'price', 'district', 'street', 'house_number', 'underground', 'residential_complex']
+selected_columns = exel_export[columns]
+selected_columns.to_csv('cian.csv', mode='a', header=False, index=False)
